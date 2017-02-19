cd8a4a8df6b7023ee8645a7d108a4d54d2810c62
Hopesresolve <gonzales.phil@hotmail.com>
2017-02-18 18:39:39 -0800

    InventorySystem_Base FunctionalityComplete -can item stack -can touch and move items -can touch and use items via touching the check mark at top right of item slot -NO COMPILER ERRORS PUSHED

---
+++ Assets/Phillip/Scripts/Item_Temp.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ItemType { CONSUMABLE, EQUIPABLE, CASTABLE };
+
+/*
+super basic item class, its set up to allow me to test my 
+inventory system functionality as well as debugging
+while jeremy creates the perma item class
+*/
+public class Item_Temp : MonoBehaviour
+{
+    public ItemType type;
+
+    public Sprite spriteNeutral;
+    public Sprite spriteHighlighted;
+
+    public int maxSize;
+
+    public void UseItem()
+    {
+        switch (type)
+        {
+            case ItemType.CONSUMABLE:
+                Debug.Log("I Just used a consumable");
+                break;
+            case ItemType.EQUIPABLE:
+                Debug.Log("I just used a consumable");
+                break;
+            case ItemType.CASTABLE:
+                Debug.Log("I just used an ability");
+                break;
+        }
+    }
+}
