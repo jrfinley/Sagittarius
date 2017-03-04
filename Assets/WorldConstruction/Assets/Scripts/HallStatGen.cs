9e83dec78aa128ff07fafbde076e8aa7406984c1
Steven Moreno <stevenxmoreno@gmail,com>
2017-03-04 13:56:49 -0800

    Foundation for dungeon gen, now it needs to be chipped away at and refined into what we need

---
+++ Assets/WorldConstruction/Assets/Scripts/HallStatGen.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HallStatGen : MonoBehaviour 
+{
+	public float requiredStr;
+	public float requiredInt;
+	public float requiredDex;
+
+	public bool isEmpty;
+	public bool isDoor;
+	public bool isHinge;
+	public bool isLock;
+
+	public GameObject thisLock;
+	public GameObject thisHinge;
+	public GameObject thisDoor; 
+
+	void Start () 
+	{
+		decideHallway();
+	}
+
+	void decideHallway()
+	{
+		isEmpty = Random.value >= 0.3;
+		if(isEmpty)
+		{
+
+		} 
+		else 
+		{
+            isDoor = Random.value >= 0.5;
+
+				if(isDoor)
+				{
+					requiredStr = Random.Range (5, 20);
+				}
+			else
+			{
+                isHinge = Random.value >= 0.5;
+
+				if (isHinge)
+				{
+					requiredInt = Random.Range (5, 20);
+				}
+
+				else
+				{
+                    isLock = true;
+					requiredDex = Random.Range (5, 20);
+				}	
+			
+			}
+
+		}
+	
+	}
+
+}
