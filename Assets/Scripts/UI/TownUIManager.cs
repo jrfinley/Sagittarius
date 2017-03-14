b9ccdffa14af5f0d84f72b86e7d26bb306aa4203
Aidan Lawrence <aidan@aidanlawrence.com>
2017-03-14 14:39:26 -0700

    Added new "Town" UI. Functionality is limited for now - waiting for save-system to transfer over inventory. Also added a player toggle system so we can add individualized stats and armor screens in the future

---
+++ Assets/Scripts/UI/TownUIManager.cs
@@ -0,0 +1,37 @@
+﻿//Aidan Lawrence
+//Controls the "Town scene" UI
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TownUIManager : MonoBehaviour
+{
+    public GameObject[] panels; //0 - Main Menu, 1 - Forge
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    void CloseAllPanels()
+    {
+        foreach(GameObject p in panels)
+        {
+            p.SetActive(false);
+        }
+    }
+
+    public void ShowMainMenuPanel()
+    {
+        CloseAllPanels();
+        panels[0].SetActive(true);
+    }
+
+    public void ShowForgePanel()
+    {
+        CloseAllPanels();
+        panels[1].SetActive(true);
+    }
+
+}
