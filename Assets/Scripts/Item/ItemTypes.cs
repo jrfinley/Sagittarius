c3d89e52d4274a8a2ddce5fd9d35bcb686880a65
Jeremy Cortez <sirjeremycortez@gmail.com>
2017-03-26 06:53:05 -0700

    Added ItemTypes to Item, updated affected scripts

---
+++ Assets/Scripts/Item/ItemTypes.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemTypes {
+    #region Varaibles
+    private EItemType itemType = EItemType.AMULET;
+    private EEquipmentType equipmentType = EEquipmentType.AMULET;
+    private EItemEquipSlot equipSlot = EItemEquipSlot.NONE;
+    private EItemWeightClass weightClass = EItemWeightClass.NONE;
+    private EWeaponDamageType damageType = EWeaponDamageType.NONE;
+    private EWeaponRange weaponRange = EWeaponRange.NONE;
+    private EItemRarity itemRarity = EItemRarity.COMMON;
+
+
+    #endregion
+
+    #region Properties
+    public EItemType ItemType {
+        get {
+            return itemType;
+        }
+        set {
+            itemType = value;
+        }
+    }
+    public EEquipmentType EquipmentType {
+        get {
+            return equipmentType;
+        }
+        set {
+            equipmentType = value;
+        }
+    }
+    public EItemEquipSlot EquipSlot {
+        get {
+            return equipSlot;
+        }
+        set {
+            equipSlot = value;
+        }
+    }
+    public EItemWeightClass WeightClass {
+        get {
+            return weightClass;
+        }
+        set {
+            weightClass = value;
+        }
+    }
+    public EWeaponDamageType DamageType {
+        get {
+            return damageType;
+        }
+        set {
+            damageType = value;
+        }
+    }
+    public EWeaponRange WeaponRange {
+        get {
+            return weaponRange;
+        }
+        set {
+            weaponRange = value;
+        }
+    }
+    public EItemRarity ItemRarity {
+        get {
+            return itemRarity;
+        }
+        set {
+            itemRarity = value;
+        }
+    }
+
+    #endregion
+
+    #region Contructors
+    public ItemTypes() { }
+    public ItemTypes(EItemType itemType, EEquipmentType equipmentType, EItemEquipSlot equipSlot, EItemWeightClass weightClass,
+            EWeaponDamageType damageType, EWeaponRange weaponRange, EItemRarity itemRarity) {
+        this.itemType = itemType;
+        this.equipmentType = equipmentType;
+        this.equipSlot = equipSlot;
+        this.weightClass = weightClass;
+        this.damageType = damageType;
+        this.weaponRange = weaponRange;
+        this.itemRarity = itemRarity;
+    }
+    public ItemTypes(ItemTypes itemTypes) {
+        this.itemType = itemTypes.ItemType;
+        this.equipmentType = itemTypes.EquipmentType;
+        this.equipSlot = itemTypes.EquipSlot;
+        this.weightClass = itemTypes.WeightClass;
+        this.damageType = itemTypes.DamageType;
+        this.weaponRange = itemTypes.WeaponRange;
+        this.itemRarity = itemTypes.ItemRarity;
+    }
+    #endregion
+}
