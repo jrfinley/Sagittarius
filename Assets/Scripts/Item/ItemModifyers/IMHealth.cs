6f4a30c0503650a00324b3fe387baf449d08f848
Jeremy Cortez <sirjeremycortez@gmail.com>
2017-03-29 10:32:07 -0700

    Started prefix and suffix names

---
+++ Assets/Scripts/Item/ItemModifyers/IMHealth.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IMHealth : AItemModifyer {
+    public IMHealth() {
+        Initialize(EItemModifyer.HEALTH,
+            new ItemStats(0, 1, 0, 0, 0, 0, 0, 0, 0),
+            new string[] { "Sturdy", "Hardy", "Tough" },
+            new string[] { "of Sturdyness", "of Swoleness", "of Ripped Abs" }
+            );
+    }
+}
