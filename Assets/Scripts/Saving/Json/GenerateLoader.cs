5e32d0a056c6878cda1400990c68f1d33ae76cde
Unknown <gonzales.phil@hotmail.com>
2017-04-19 09:53:13 -0700

    Json Update

---
+++ Assets/Scripts/Saving/Json/GenerateLoader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateLoader : MonoBehaviour
+{
+    private GameController gameController;
+    private int count = 0;
+
+	void Start ()
+    {
+        gameController = FindObjectOfType<GameController>();
+        Destroy(gameObject, .22f);
+
+        StartCoroutine(CallLoaderSpawn());
+    }
+
+    IEnumerator CallLoaderSpawn()
+    {
+        yield return new WaitForSeconds(.21f);
+        gameController.Load();
+        Destroy(gameObject, 1f);
+        /*
+            if (count < 1)
+            {
+                count++;
+                Debug.Log("Created Loader" + count + " Times");
+            }
+            else
+            {
+            }
+        }
+        */
+    }
+
+}
