3adb259952a0cf8965f6af2c631a40bfed7d7a19
Aidan Lawrence <aidan@aidanlawrence.com>
2017-04-26 10:11:15 -0700

    Added a new tutorial scene. Added first camera animation.

---
+++ Assets/Scripts/Tutorial/OpeningCameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpeningCameraController : MonoBehaviour
+{
+    Transition transition;
+    void Awake()
+    {
+        transition = GameObject.FindGameObjectWithTag("Transition").GetComponent<Transition>();
+    }
+
+    public void FadeInTransition() //CALLED BY CAMERA ANIMATION
+    {
+        transition.StartFade(Color.clear, 2f);
+    }
+    public void FadeOutTransition() //CALLED BY CAMERA ANIMATION
+    {
+        transition.StartFade(new Color(0.05882f, 0f, 0.04314f, 1f), 1f);
+    }
+}
