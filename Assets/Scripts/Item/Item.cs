3fe7603c9b08946e10810102cff2849b03829979
Jeremy Cortez <sirjeremycortez@gmail.com>
2017-05-01 10:12:51 -0700

    Fixed reforge and ig now stores base values

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -8,6 +8,9 @@ public class Item {
     private string flavorText = string.Empty;
     private string iconName = string.Empty; //Why a string? It is so we can utilize the Resources folder, using refrences could cause crashes while in development (ie refrencing something that doesn't exist).
     private int itemLevel = 0;
+    private int prefixIndex = 0;
+    private int suffixIndex = 0;
+    private int seed = 0;
     private ItemTypes itemTypes;
     private ItemStats itemStats;
     private bool statProtection = true;
@@ -57,6 +60,24 @@ public class Item {
         set {
             if(!statProtection)
                 itemLevel = value;
+        }
+    }
+    public int PrefixIndex {
+        get {
+            return prefixIndex;
+        }
+        set {
+            if(!statProtection)
+                prefixIndex = value;
+        }
+    }
+    public int SuffixIndex {
+        get {
+            return suffixIndex;
+        }
+        set {
+            if(!statProtection)
+                suffixIndex = value;
         }
     }
     public ItemTypes Types {
--- Assets/Scripts/ItemGenerator/ItemStatsCalculator.cs
+++ Assets/Scripts/ItemGenerator/ItemStatsCalculator.cs
@@ -8,7 +8,6 @@ namespace ItemGeneratorHelpers {
         public ItemStats CalculateItemStats(ItemStats baseStats, EItemRarity itemRarity, int itemLevel) {
             baseStats.AddToAll((float)itemRarity * 2f);
             baseStats.MultiplyByAll(Mathf.Sqrt(itemLevel + 1) + 1);
-            baseStats.MultiplyByAll(Random.Range(.95f, 1.2f));
             return new ItemStats(baseStats);
         }
     }
