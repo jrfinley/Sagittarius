d5ee6dc49e637361bb1866a9d08f5e451fde3415
Jeremy Cortez <sirjeremycortez@gmail.com>
2017-06-01 06:39:22 -0700

    Renamed Item folder to Items

---
+++ Assets/Scripts/Items/ItemTypes.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemTypes {
+    #region Varaibles
+    private EItemType itemType = EItemType.ACCESSORY;
+    private EEquipmentType equipmentType = EEquipmentType.AMULET;
+    private EItemEquipSlot equipSlot = EItemEquipSlot.NONE;
+    private EItemWeightClass weightClass = EItemWeightClass.NONE;
+    private EWeaponDamageType damageType = EWeaponDamageType.NONE;
+    private EWeaponRange weaponRange = EWeaponRange.NONE;
+    private EItemRarity itemRarity = EItemRarity.COMMON;
+    private EItemModifyer prefixItemModifyer = EItemModifyer.NONE;
+    private EItemModifyer suffixItemModifyer = EItemModifyer.NONE;
+    #endregion
+
+    #region Properties
+    public EItemType ItemType {
+        get {
+            return itemType;
+        }
+        set {
+            itemType = value;
+        }
+    }
+    public EEquipmentType EquipmentType {
+        get {
+            return equipmentType;
+        }
+        set {
+            equipmentType = value;
+        }
+    }
+    public EItemEquipSlot EquipSlot {
+        get {
+            return equipSlot;
+        }
+        set {
+            equipSlot = value;
+        }
+    }
+    public EItemWeightClass WeightClass {
+        get {
+            return weightClass;
+        }
+        set {
+            weightClass = value;
+        }
+    }
+    public EWeaponDamageType DamageType {
+        get {
+            return damageType;
+        }
+        set {
+            damageType = value;
+        }
+    }
+    public EWeaponRange WeaponRange {
+        get {
+            return weaponRange;
+        }
+        set {
+            weaponRange = value;
+        }
+    }
+    public EItemRarity Rarity {
+        get {
+            return itemRarity;
+        }
+        set {
+            itemRarity = value;
+        }
+    }
+    public EItemModifyer PrefixModifyer {
+        get {
+            return prefixItemModifyer;
+        }
+        set {
+            prefixItemModifyer = value;
+        }
+    }
+    public EItemModifyer SuffixModifyer {
+        get {
+            return suffixItemModifyer;
+        }
+        set {
+            suffixItemModifyer = value;
+        }
+    }
+    #endregion
+
+    #region Contructors
+    public ItemTypes() { }
+    public ItemTypes(EItemType itemType, EEquipmentType equipmentType, EItemEquipSlot equipSlot, EItemWeightClass weightClass,
+            EWeaponDamageType damageType, EWeaponRange weaponRange, EItemRarity itemRarity = EItemRarity.COMMON, 
+            EItemModifyer prefixItemModifyer = EItemModifyer.NONE, EItemModifyer suffixItemModifyer = EItemModifyer.NONE) {
+        this.itemType = itemType;
+        this.equipmentType = equipmentType;
+        this.equipSlot = equipSlot;
+        this.weightClass = weightClass;
+        this.damageType = damageType;
+        this.weaponRange = weaponRange;
+        //Following have defaults
+        this.itemRarity = itemRarity;
+        this.prefixItemModifyer = prefixItemModifyer;
+        this.suffixItemModifyer = suffixItemModifyer;
+    }
+    public ItemTypes(ItemTypes types) {
+        itemType = types.ItemType;
+        equipmentType = types.EquipmentType;
+        equipSlot = types.EquipSlot;
+        weightClass = types.WeightClass;
+        damageType = types.DamageType;
+        weaponRange = types.WeaponRange;
+        itemRarity = types.Rarity;
+        prefixItemModifyer = types.PrefixModifyer;
+        suffixItemModifyer = types.SuffixModifyer;
+    }
+    #endregion
+}
