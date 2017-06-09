6fac8dab06675fe5bdb33b4528b3fbe31d6cf685
Jeremy Cortez <sirjeremycortez@gmail.com>
2017-06-09 12:17:55 -0700

    Added droptables (untested, miracle if works first try)

---
+++ Assets/Scripts/Items/DropTable/DropTable.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class DropTable {
+    //a drop table is the droptable for each monster's drop values
+    private Dictionary<EMonsterType, DropTableElement[]> dropTable;
+    private EEquipmentType defaultItem = EEquipmentType.AMULET; //for debugging
+
+    public DropTable(Dictionary<EMonsterType, DropTableElement[]> dropTable) {
+
+    }
+
+    public EEquipmentType GetItemDrop(EMonsterType monster) {
+        if(!IsIncluded(monster))
+            return defaultItem;
+
+        float dropValue = Random.value;
+        int dropIndex = 0;
+        int dteLength = dropTable[monster].Length;
+
+        for(; dropIndex < dteLength; dropIndex++) {
+            //DropTableElement.dropChance is a value from 0-1, calculated from dropweight when it was created
+            //when dropValue is <= 0, then it is within that items drop chance range
+            //this is basically creating ranges for each of the dropChances withough having them as actual ranges, which would take 2 values (min, max)
+            dropValue -= dropTable[monster][dropIndex].dropChance;
+            if(dropValue <= 0)
+                break;
+        }
+
+        return dropTable[monster][dropIndex].item;
+    }
+
+    private bool IsIncluded(EMonsterType monster) {
+        if(dropTable.ContainsKey(monster))
+            return true;
+        Debug.LogError("Monster (" + monster + ") is not included in the DropTable!");
+        return false;
+    }
+}
---
+++ Assets/Scripts/Items/DropTable/DropTableElement.cs
@@ -0,0 +1,9 @@
+﻿public struct DropTableElement {
+    public EEquipmentType item;
+    public float dropChance; //value from  0-1
+
+    public DropTableElement(EEquipmentType item, float dropChance) {
+        this.item = item;
+        this.dropChance = dropChance;
+    }
+}
