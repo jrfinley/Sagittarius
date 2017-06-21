f4c96668b8493aac3ed94f22368892165f561bb8
Skyler <ockn0n@hotmail.com>
2017-06-20 23:00:42 -0700

    Room connections are now stored in dict<Enum, Transform> for more consistant manipulation, also changed how connections are stored in lists. Refactored finding closest connections from distance to enum (key) to connection (value).

--- Assets/Scripts/DungeonGeneration/Room.cs
+++ Assets/Scripts/DungeonGeneration/Room.cs
@@ -13,22 +13,39 @@ public class Room : MonoBehaviour
         public List<Transform> joinedConnections;
         public List<Transform> unusedConnections;
     }
-
+    public Room roomToFind;
     private ConnectionData _connections;
     public ConnectionData Connections { get { return _connections; } }
+
+    private Dictionary<normalRoomConnections, Transform> _directionToTransform = new Dictionary<normalRoomConnections, Transform>();
+    private enum normalRoomConnections { North, South, East, West }
 
     public Room parentRoom;
     public Vector3 position;
     public bool quadRoom;
 
-    public void Start()
+    void Start()
     {
         _connections = new ConnectionData();
         _connections.allConnections = connectionContainer.GetComponentsInChildren<Transform>().ToList();
+        _connections.allConnections.RemoveAt(0);
         _connections.unusedConnections = new List<Transform>();
+        _connections.joinedConnections = new List<Transform>();
+        _connections.blockedConnections = new List<Transform>();
 
-        foreach (Transform connection in _connections.allConnections)
-            _connections.unusedConnections.Add(connection);
+        if (_connections.allConnections != null)
+        {
+            foreach (Transform connection in _connections.allConnections)
+                _connections.unusedConnections.Add(connection);
+
+            foreach (Transform connection in _connections.allConnections)
+                _directionToTransform.Add(_GetConnectionOrientation(connection.localPosition), connection);
+        }
+
+        if (roomToFind != null)
+        {
+            Connect(roomToFind);
+        }
     }
 
     public Transform GetRandomUnusedConnection()
@@ -54,9 +71,10 @@ public class Room : MonoBehaviour
 
     private void _JoinConnection(Transform connectionToJoin)
     {
-        //_connections.connections.Remove(connectionToJoin);
-        //_connections.openConnections.Remove(connectionToJoin);
-        //_connections.joinedConnections.Add(connectionToJoin);
+        _connections.unusedConnections.Remove(connectionToJoin);
+        _connections.joinedConnections.Add(connectionToJoin);
+
+        //Debug.Log("Added joined connection: " + connectionToJoin.transform.localPosition);
     }
 
     public void Connect(Room otherRoom)
@@ -68,21 +86,55 @@ public class Room : MonoBehaviour
 
     private Transform _GetClosestTransform(Room otherRoom)
     {
-        //float closest = 100f;
-        //float dist;
-        //Transform closestTransform = null;
-        //foreach (Transform connection in _connections.connections)
-        //{
-        //    dist = Vector3.Distance(connection.position, otherRoom.position);
-        //    if (dist < closest)
-        //    {
-        //        closestTransform = connection;
-        //        closest = dist;
-        //    }
-        //}
-        //return closestTransform;
-        return null;
+        normalRoomConnections direction = _GetDirectionToRoom(otherRoom);
+        //Debug.Log("Closest Transform is: " + direction);
+        return _directionToTransform[direction];
     }
+
+    private normalRoomConnections _GetDirectionToRoom(Room otherRoom)
+    {
+        Vector3 direction = -1 * (otherRoom.transform.position - this.transform.position);
+        return _GetConnectionOrientation(direction);
+    }
+
+    private normalRoomConnections _GetConnectionOrientation(Vector3 direction)
+    {
+        //north - up and right
+        //south - down and left
+        //east - down and right
+        //west - up and left
+        //inverted north/south to compensate for Quad rotations
+
+        if (direction.x > 0 && direction.z > 0)
+            return normalRoomConnections.South;
+        else if (direction.x < 0 && direction.z < 0)
+            return normalRoomConnections.North;
+        else if (direction.x < 0 && direction.z > 0)
+            return normalRoomConnections.East;
+        else if (direction.x > 0 && direction.z < 0)
+            return normalRoomConnections.West;
+
+        Debug.LogError("Couldn't find the connection direction, defaulting to North");
+        return normalRoomConnections.North;
+    }
+
+    //private Vector3 _GetDirection(Room otherRoom)
+    //{
+    //    Vector3 secondRoom = otherRoom.gameObject.transform.position;
+    //    Vector3 direction = secondRoom - this.transform.position;
+
+    //    if (direction.x > 0 && direction.z > 0)
+    //        return new Vector3(1, 0, 1);
+    //    else if (direction.x < 0 && direction.z < 0)
+    //        return new Vector3(-1, 0, -1);
+    //    else if (direction.x < 0 && direction.z > 0)
+    //        return new Vector3(-1, 0, 1);
+    //    else if (direction.x > 0 && direction.z < 0)
+    //        return new Vector3(1, 0, -1);
+
+    //    Debug.Log("Couldn't get direction, returning Vector3.zero");
+    //    return Vector3.zero;
+    //}
 
     public void BecomeMonsterRoom()
     {
@@ -97,21 +149,5 @@ public class Room : MonoBehaviour
     public void BecomeStatCheck()
     {
         gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
-    }
-
-    private Vector3 _GetDirection(Vector3 direction)
-    {
-        Vector3 dir = Vector3.zero;
-
-        if (direction.x > 0)
-            dir.x += 0.25f;
-        else if (direction.x < 0)
-            dir.x -= 0.25f;
-        if (direction.z > 0)
-            dir.z += 0.25f;
-        else if (direction.z < 0)
-            dir.z -= 0.25f;
-
-        return dir;
     }
 }
